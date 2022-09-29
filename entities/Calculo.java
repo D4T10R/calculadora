f32a4a493784849d85710040a64c00fd7113adca
Datior <leonardowederveiga@yahoo.com>
2022-09-29 12:03:56 -0300

    Feito a opção um ate opcao quatro

--- entities/Calculo.java
+++ entities/Calculo.java
@@ -2,32 +2,36 @@ package entities;
 
 import java.util.Scanner;
 
+import javax.print.attribute.standard.RequestingUserName;
+
+import java.lang.Math;
+
 public class Calculo {
     
-    private int v1;
-    private int v2;
+    private double v1;
+    private double v2;
 
     // CONSTRUTORES
     public Calculo (){
     }
 
-    public Calculo(int valor1, int valor2) {
+    public Calculo(Double valor1, Double valor2) {
         this.v1 = valor1;
         this.v2 = valor2;
     }
     // CONSTRUTORES
 
     // ENCAPSULAMENTO
-    public int getV1() {
+    public double getV1() {
         return v1;
     }
-    public void setV1(int v1) {
+    public void setV1(Double v1) {
         this.v1 = v1;
     }
-    public int getV2() {
+    public double getV2() {
         return v2;
     }
-    public  void setV2(int v2, int i) {
+    public  void setV2(Double v2) {
         this.v2 = v2;
     }
     // ENCAPSULAMENTO
@@ -40,14 +44,16 @@ public class Calculo {
         
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("------------------------------------------------------");
+        System.out.println("\n------------------------------------------------------");
         System.out.println("-----------------calculadora do datior----------------");
         System.out.println("------------------------------------------------------");
         System.out.print("\nPrimeiro numero: ");
-        setV1(sc.nextInt());
+        setV1(sc.nextDouble());
         System.out.print("\nSegundo numero: ");
-        setV2(sc.nextInt(), v2);
-        System.out.println("***** Selecione uma operação que deseja realizar *****");    
+        setV2(sc.nextDouble());
+        System.out.println("\n------------------------------------------------------");
+        System.out.println("***** Selecione uma operação que deseja realizar *****");
+        System.out.println("------------------------------------------------------");    
         System.out.println("\n|   Opção 1 - Somar         | "+
         "\n|   Opção 2 - Subtrair      |" +
         "\n|   Opção 3 - Multiplicar   |" +
@@ -55,23 +61,62 @@ public class Calculo {
         "\n|   Opção 5 - Elevação      |" +
         "\n|   Opção 6 - Raiz quadrada |" +
         "\n|   Opção 7 - Sair          |");
+        System.out.print("Qual sua opção? ");
         int selecao = sc.nextInt();
 
         switch(selecao) {
             case 1:
-                int soma = getV1() + getV2();
-                System.out.printf("A soma entre %d + %d =", getV1(), getV2(), soma);
-                return soma;
-                break;
+                Double resultado = somaNum(getV1(), getV2());
+                System.out.printf("\nA soma entre %.2f + %.2f = %.2f\n", getV1(), getV2(), resultado);
+                return operacao();
+               
             case 2:
-
-
+                resultado = subNum(getV1(), getV2());
+                System.out.printf("A subtração entre %.2f - %.2f = %.2f", getV1(), getV2(), resultado);
+                return operacao();
+               
+            case 3: 
+                resultado = multNum(getV1(), getV2());
+                System.out.printf("A multiplicação entre %.2f x %.2f = %.2f", getV1(), getV2(), resultado);
+                return operacao();
+               
+            case 4: 
+                resultado = divNum(getV1(), getV2());
+                System.out.printf("A divisao entre %.2f / %.2f = %.2f", getV1(), getV2(), resultado);
+                return operacao();
+                
+            case 5: 
+                resultado = elevNum(getV1(), getV2());
+                System.out.printf("A elevação de %.2f ^ %.2f = %.2f", getV1(), getV2(), resultado);
+                return operacao();
+                
+            default: 
+                System.out.println("Erro");
+                break;
         }
 
 
         return 1;
     }
-    
 
-   
+    public Double somaNum(Double v1, Double v2) {
+        return v1 + v2;
+    }
+    
+    public Double subNum(Double v1, Double v2) {
+        return v1 - v2;
+    }
+
+    public Double multNum(Double v1, Double v2) {
+        return v1 * v2;
+    }
+    
+    public double divNum(Double v1, Double v2) {
+        return v1 / v2;
+    }
+
+    public Double elevNum(Double v1, Double v2) {
+        return Math.pow(v1, v2);
+    }
+
 }
